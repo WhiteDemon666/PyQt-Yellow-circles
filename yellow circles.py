e9044ddb0664c5e26f7d537d5c3996283ffc3ece
Whisse <43343261+WhiteDemon666@users.noreply.github.com>
2022-12-23 00:47:06 +0300

    Add files via upload

--- yellow circles.py
+++ yellow circles.py
@@ -1,14 +1,37 @@
+from PyQt5 import QtCore, QtWidgets
 import sys
 import random
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(832, 626)
+        MainWindow.setMinimumSize(QtCore.QSize(832, 626))
+        MainWindow.setMaximumSize(QtCore.QSize(832, 626))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setMinimumSize(QtCore.QSize(832, 626))
+        self.centralwidget.setObjectName("centralwidget")
+        self.btn = QtWidgets.QPushButton(self.centralwidget)
+        self.btn.setGeometry(QtCore.QRect(350, 270, 91, 31))
+        self.btn.setObjectName("btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.btn.setText(_translate("MainWindow", "Нажми меня"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('yellow circles.ui', self)
+        self.setupUi(self)
         self.btn.clicked.connect(self.run)
         self.do_paint = False
 
@@ -27,8 +50,9 @@ class MyWidget(QMainWindow):
             qp.end()
 
     def draw(self, qp):
-        size = random.randint(1, 200)
-        qp.setBrush(QColor('yellow'))
+        size = random.randint(10, 200)
+        color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+        qp.setBrush(QColor(*color))
         qp.drawEllipse(random.randint(size, 832 - size), random.randint(size, 626 - size), size, size)
 
 
